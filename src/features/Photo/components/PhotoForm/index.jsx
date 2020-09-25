f73077d3b9b34a43aace589a071a099f33fdf1ea
thinhgakon <toi.la.dan.xu.nghe@gmail.com>
2020-09-25 10:26:50 +0700

    Custom control Formik: random photo

--- src/features/Photo/components/PhotoForm/index.jsx
+++ src/features/Photo/components/PhotoForm/index.jsx
@@ -1,12 +1,11 @@
 import { PHOTO_CATEGORY_OPTIONS } from "constants/global";
-import Images from "constants/images";
+import InputField from "custom-fields/InputField";
+import RandomPhotoField from "custom-fields/RandomPhotoField";
+import SelectField from "custom-fields/SelectField";
+import { FastField, Form, Formik } from "formik";
 import PropTypes from "prop-types";
 import React from "react";
-import { FastField, Field, Form, Formik, useFormik } from "formik";
-import Select from "react-select";
-import { Button, FormGroup, Input, Label } from "reactstrap";
-import InputField from "custom-fields/InputField";
-import SelectField from "custom-fields/SelectField";
+import { Button, FormGroup } from "reactstrap";
 
 PhotoForm.propTypes = {
   onSubmit: PropTypes.func,
@@ -48,23 +47,7 @@ function PhotoForm(props) {
           options={PHOTO_CATEGORY_OPTIONS}
         />
 
-        <FormGroup>
-          <Label for="categoryId">Photo</Label>
-
-          <div>
-            <Button type="button" outline color="primary">
-              Random a photo
-            </Button>
-          </div>
-          <div>
-            <img
-              width="200px"
-              height="200px"
-              src={Images.COLORFUL_BG}
-              alt="colorful background"
-            />
-          </div>
-        </FormGroup>
+        <FastField name="photo" component={RandomPhotoField} label="Photo" />
 
         <FormGroup>
           <Button color="primary">Add to album</Button>
